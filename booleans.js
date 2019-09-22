6dfedffa7084c659833157ac10ffcb504a15a63b
hadilhassouna <53368223+hadilhassouna@users.noreply.github.com>
2019-09-22 08:04:12 -0700

    Add files via upload

---
+++ booleans.js
@@ -0,0 +1,45 @@
+function oldEnoughToDrive(age) {
+	if (age >= 16) {
+		return "true";
+	}
+	return "false";
+}
+
+function sameLength(str1, str2) {
+	if (str1.length = str2.lenght) {
+		return "True";
+	}
+	return "False";
+}
+
+function passwordLongEnough (pass) {
+	if (pass.length = 12) {
+		return "True";
+	}
+	return "False";
+}
+
+
+function rentalCar(name, age) {
+	if (age >= 21) {
+		return "Have fun driving";
+	}
+	return "You cannot have the keys";
+}
+
+function max (num1, num2){
+	if (num1>num2) {
+		return num1;
+
+	}
+	return num2;
+}
+
+function min (num1, num2){
+	if (num1>num2) {
+		return num2;
+
+	}
+	return num1;
+}
+
